faa3aed6d0c56de76f19c2900ac538feea33b436
Mridul Ojha <mridul.ojha@bhnetowrk.com>
2024-03-07 11:37:54 +0530

    Added comments for basic app understanding

--- angular-essentials/src/app/app.component.ts
+++ angular-essentials/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // CSS Selector that tell Angular which element to select; should be unique
   selector: 'app-root',
+  // render the below component for the above selector
   templateUrl: './app.component.html',
+  // render the below CSS for the above template
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
