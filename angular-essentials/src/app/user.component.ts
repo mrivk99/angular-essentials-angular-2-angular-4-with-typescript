1385661478f4df35acad193adf3f42dc40779f85
Mridul Ojha <mridul.ojha@bhnetowrk.com>
2024-03-07 14:58:15 +0530

    Added custom component

---
+++ angular-essentials/src/app/user.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>User details page</p>
+    <p>I'm the user component</p>`
+})
+
+export class UserComponent {
+}
