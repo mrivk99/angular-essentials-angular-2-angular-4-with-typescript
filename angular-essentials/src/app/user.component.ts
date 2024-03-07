51745cbe95a384d778c0a911746535243b6fa4a4
Mridul Ojha <mridul.ojha@bhnetowrk.com>
2024-03-07 15:29:18 +0530

    Added content to template via string interpolation

--- angular-essentials/src/app/user.component.ts
+++ angular-essentials/src/app/user.component.ts
@@ -3,9 +3,10 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-user',
   template: `
-    <p>User details page</p>
+    <p>Hello {{name}}</p>
     <p>I'm the user component</p>`
 })
 
 export class UserComponent {
+  name='Ben'
 }
